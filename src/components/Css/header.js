88925e0fbf8ebf91b474661d652cd5c70dfefe7b
ayeshasheikh25 <ayesha25090204@gmail.com>
2026-08-21 16:05:26 +0500

    Delete src directory

--- src/components/Css/header.js
+++
@@ -1,6 +0,0 @@
-export const header ={
-    container: 'flex justify-between items-center p-2 text-sm',
-    search: 'bg-gray-100 flex justify-around items-center p-[8px] w-96 rounded-3xl',  
-    input: ' w-10/12 p-1 border-none outline-none bg-transparent',
-    dropdown: 'text-base capitalize font-semibold cursor-pointer'
-}
--- src/navigation/NavDropdown.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NavDropdown() {
-  return (
-    <div>NavDropdown</div>
-  )
-}
-
-export default NavDropdown
